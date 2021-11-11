a9c9714e3afec7b0a4f2040cadc2e0ddb94c20f5
code <hi@sammagafu.com>
2021-11-11 16:06:09 +0300

    added the django project to github

---
+++ pages/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+# Create your views here.
+
+
+class Homepage(TemplateView):
+    template_name = "pages/index.html"
+
+
